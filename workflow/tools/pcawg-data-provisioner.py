bbd30d55e5a174ab43ab0b472f29e34fdfd4a127
Junjun Zhang <junjun.ca@gmail.com>
2018-04-16 16:22:51 -0400

    code reorganization and simplification

--- workflow/tools/pcawg-data-provisioner.py
+++ workflow/tools/pcawg-data-provisioner.py
@@ -2,42 +2,46 @@
 
 import os
 import sys
-from utils import get_task_dict, save_output_json
-from pcawg_workflow.utils import hyphen_to_camel_case
-from pcawg_workflow.pcawg_cocleaning import PcawgCocleaning
-from pcawg_workflow.pcawg_oxog_filter import PcawgOxogFilter
-from pcawg_workflow.pcawg_snv_indel_annotation import PcawgSnvIndelAnnotation
+import requests
+import zipfile
+from io import BytesIO
+
+from utils import get_task_dict, save_output_json, get_input_path, get_output_path, get_reference_path
 
 
 task_dict = get_task_dict(sys.argv[1])
 cwd = os.getcwd()
 
-workflow_name = task_dict.get('input').get('workflow_name')
-workflow_version = task_dict.get('input').get('workflow_version')
-workflow_type = task_dict.get('input').get('workflow_type')
-metadata = task_dict.get('input').get('metadata')
+workflow_name = task_dict.get('input').get('workflow').get('name')
+workflow_version = task_dict.get('input').get('workflow').get('version')
+workflowfile_name = task_dict.get('input').get('workflow').get('workflowfile_name')
+workflow_type = task_dict.get('input').get('workflow').get('type')
+repo_url = task_dict.get('input').get('workflow').get('repo_url')
+pipeline = task_dict.get('input').get('metadata').get('pipeline')
+job_partiption_key = task_dict.get('input').get('metadata').get('job_partiption_key')
 metadata_service = task_dict.get('input').get('metadata_service')
-local_path = task_dict.get('input').get('local_path')
-output = task_dict.get('input').get('output')
-job_file_template = task_dict.get('job_file_template')
+job_file_template = task_dict.get('input').get('job_file_template')
 
-workflow_class_name = hyphen_to_camel_case(workflow_name)
 
-provisioner_class = eval(workflow_class_name)
-provisioner = provisioner_class(
-                                workflow_version=workflow_version,
-                                workflow_type=workflow_type,
-                                metadata=metadata,
-                                metadata_service=metadata_service,
-                                local_path=local_path,
-                                output=output,
-                                job_file_template=job_file_template
-                                )
-provisioner.provision_files()
+## TODO: start from job_file_template, download necessary input (and reference) data and populate
+## job JSON fields with concrete values
+input_path = get_input_path(pipeline, job_partiption_key, workflow_name, workflow_version)
+reference_path = get_reference_path(pipeline, job_partiption_key, workflow_name, workflow_version)
+
+
+# write out job JSON
+job_file = 'job.json'
+
+
+git_download_url = "%s/archive/%s.zip" % (repo_url, workflow_version)
+request = requests.get(git_download_url)
+zfile = zipfile.ZipFile(BytesIO(request.content))
+zfile.extractall(os.getcwd())
+
 
 output_json = {
-    'job_file': provisioner.job_file,
-    'wf_file': provisioner.wf_file
+    'job_file': job_file,
+    'wf_file': os.path.join(os.getcwd(), "%s-%s" % (workflow_name, workflow_version), workflowfile_name)
 }
 
 save_output_json(output_json)
---
+++ workflow/tools/utils/__init__.py
@@ -0,0 +1,63 @@
+import os
+import json
+
+
+def get_task_dict(json_string):
+    try:
+        task_dict = json.loads(json_string)
+    except:
+        return {}
+
+    return task_dict
+
+
+def save_output_json(output_dict={}):
+    with open('output.json', 'w') as f:
+        f.write(json.dumps(output_dict, indent=2))
+
+
+def get_workdir():
+    return os.getcwd()
+
+
+def get_workflow_path(pipeline, job_partiption_key, workflow_name, workflow_version):
+    """
+    jthome
+    ├── account.2eea3942-2c9c-4cdb-ad6f-606d062730e5
+    └── data
+        └── pipeline.PCAWG  # pipeline
+            └── DO36881  # job_partiption_key
+                └── pcawg-oxog-filter  # workflow_name
+                    └── 1.0.0  # workflow_version, THIS IS WORKFLOW_PATH
+    """
+    if pipeline is None or job_partiption_key is None or workflow_name is None or workflow_version is None:
+        exit(1)
+
+    if len(pipeline) == 0 or len(job_partiption_key) == 0 or len(workflow_name) == 0 or len(workflow_version) == 0:
+        exit(1)
+
+    # get current working direcotry first
+    cwd = get_workdir()
+    path_list = cwd.split(os.sep)
+    while not path_list.pop().startswith('account.'):
+        pass
+
+    if len(path_list) == 0:
+        exit(1)  # should never happen
+
+    jthome = os.sep.join(path_list)
+    return os.sep.join(jthome + ['data', 'pipeline.%s' % pipeline,
+                                job_partiption_key, workflow_name,
+                                workflow_version])
+
+
+def get_input_path(pipeline, job_partiption_key, workflow_name, workflow_version):
+    return os.sep.join([get_workflow_path(pipeline, job_partiption_key, workflow_name, workflow_version), 'input'])
+
+
+def get_output_path(pipeline, job_partiption_key, workflow_name, workflow_version):
+    return os.sep.join([get_workflow_path(pipeline, job_partiption_key, workflow_name, workflow_version), 'output'])
+
+
+def get_reference_path(pipeline, job_partiption_key, workflow_name, workflow_version):
+    return os.sep.join([get_workflow_path(pipeline, job_partiption_key, workflow_name, workflow_version), 'reference'])
